12dd1a277b9a8469049b4cf2480bc05a022a5e2d
Robson Teixeira <robson.teixeira2000@gmail.com>
2020-11-10 21:30:28 -0300

    Adicionado novos tokens

--- verificador_de_corretude/lexer.py
+++ verificador_de_corretude/lexer.py
@@ -13,7 +13,7 @@ class Lexer():
         self.lexer.add('OPEN_PAREN', r'\(')
         self.lexer.add('CLOSE_PAREN', r'\)')
 
-        # Connectives and rules
+        # Connectives
         self.lexer.add('NOT', r'\~')
         self.lexer.add('IMPLIE', r'\->')
         self.lexer.add('AND', r'\&')
@@ -21,11 +21,27 @@ class Lexer():
         self.lexer.add('RAA', r'raa')
         self.lexer.add('BOTTOM', r'\@')
 
+        #rules
+        #TODO implie elimination
+        #TODO implie introduction
+        #TODO or elimination
+        #TODO or introduction
+        #TODO And elimination
+        #TODO And introduction
+        self.lexer.add('NEG_INTROD', r'\~i')
+        self.lexer.add('NEG_ELIM', r'\~e')
+        self.lexer.add('RAA', r'raa')
+        self.lexer.add('BOTTOM', r'\@')
+
         # Number
         self.lexer.add('NUM', r'\d+')
 
-        # Proposition
-        self.lexer.add('PROPOSITION', r'[a-zA-Z][a-zA-Z0-9]*' )
+        # Athom
+        self.lexer.add('ATHOM', r'[a-zA-Z][a-zA-Z0-9]*' )
+
+        #justification
+        #TODO hypothesis
+        #TODO premise
 
         # Ignore spaces
         self.lexer.ignore('\s+')
