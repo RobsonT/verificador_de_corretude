03dc6c2563f631d96e03b611d95b2ac43689ceb1
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-11-16 10:15:04 -0300

    Receiving demonstrations from terminal

--- verificador_de_formulas/main.py
+++ verificador_de_formulas/main.py
@@ -1,9 +1,11 @@
 from lexer import Lexer
 from analisys import Parser
+import sys
 
-text_input = """1. Ab def A
-2. ~Ab def ~ 
-"""
+text_input = ""
+
+for param in sys.argv[1:] :
+    text_input += param
 
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
